6fb0bfaa88e9fcf442a030ac473071a7ac798c7a
lvxz5 <lvxz5@ziroom.com>
2017-03-13 14:50:18 +0800

    update

--- fiction-provider/src/main/java/com/note/provider/fiction/api/FictionApiService.java
+++ fiction-provider/src/main/java/com/note/provider/fiction/api/FictionApiService.java
@@ -8,18 +8,68 @@ import java.sql.SQLException;
  */
 public interface FictionApiService {
 
+    /**
+     * 名称检索
+     * @param json
+     * @return
+     * @throws SQLException
+     * @throws IOException
+     */
     String search(String json) throws SQLException, IOException;
 
+    /**
+     * 多功能检索
+     * @param json
+     * @return
+     * @throws SQLException
+     * @throws IOException
+     */
     String findFull(String json) throws SQLException, IOException;
 
+    /**
+     * RANK列表
+     * @param json
+     * @return
+     * @throws SQLException
+     * @throws IOException
+     */
     String findRankList(String json) throws SQLException, IOException;
 
+    /**
+     * 详情信息
+     * @param json
+     * @return
+     * @throws SQLException
+     * @throws IOException
+     */
     String findFictionInfo(String json) throws SQLException, IOException;
 
+    /**
+     * 章节列表
+     * @param json
+     * @return
+     * @throws SQLException
+     * @throws IOException
+     */
     String findFictionChapters(String json) throws SQLException, IOException;
 
+    /**
+     * 章节信息
+     * @param json
+     * @return
+     * @throws SQLException
+     * @throws IOException
+     */
     String findChapterInfo(String json) throws SQLException, IOException;
 
+    /**
+     * 最新列表  @TODO
+     *
+     * @param json
+     * @return
+     * @throws SQLException
+     * @throws IOException
+     */
     String findNewList(String json) throws SQLException, IOException;
 
 }
